bd6cc4e3aa705c4788c44be2981e91c8105cdda1
Kali <kifflyer@gmail.com>
2019-11-16 23:20:43 +0200

    First commit

---
+++ test.rb
@@ -0,0 +1,30 @@
+def main
+	file = File.new("input.txt", "w+")
+	for i in 0...50
+		file.puts -100 + rand(200)
+	end
+	file.close
+	arr = []
+	File.readlines("input.txt").each { |tmp|
+		arr.push(tmp.to_i)
+	}
+	count = 0
+	for i in 0...50
+		if arr[i] < 0
+			count += 1
+		end
+	end
+	puts "Count of negative elements of the array is equal to: #{count}"
+	file = File.new("out.txt", "w+")
+	for i in 0...50
+		if arr[i] < 0
+			file.puts "arr[#{i}]: #{arr[i]}"
+		end
+	end
+	file.puts "Count: #{count}"
+	file.close
+
+	puts "Hello, world!"
+end
+
+main
